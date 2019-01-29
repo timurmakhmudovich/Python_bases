b305b8fbf5d0b46d497710d6bba71c182acbd72a
timurmakhmudovich <46539493+timurmakhmudovich@users.noreply.github.com>
2019-01-29 13:41:43 +0300

    Update hw03_normal.py

--- Lesson_3/hw03_normal.py
+++ Lesson_3/hw03_normal.py
@@ -21,8 +21,6 @@ def sort_to_max(origin_list):
         for i in range(1, len(origin_list)):
             if origin_list[i - 1] > origin_list[i]:
                 origin_list[i - 1], origin_list[i] = origin_list[i], origin_list[i - 1]
-        j += 1
-
     return origin_list
 
 print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
