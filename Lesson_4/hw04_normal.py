c2f7f7faf1d975a351c9f387e38c04367e424b70
timurmakhmudovich <itm1988@yandex.ru>
2019-01-31 09:46:03 +0300

    Сделал normal

--- Lesson_4/hw04_normal.py
+++ Lesson_4/hw04_normal.py
@@ -4,7 +4,42 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
 
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+#1
+print(re.findall("[a-z]+", line))
+
+#2
+lst = []
+string = ""
+
+for char in line:
+    if char.islower():
+        string += char
+    elif char.isupper() and len(string) > 0:
+        lst.append(string)
+        string = ""
+if len(string) > 0:
+    lst.append(string)
+
+print(lst)
 
 # Задание-2:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
@@ -13,3 +48,31 @@
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
 
+import random
+
+
+lst = [ random.randint(0, 9) for _ in range(2500) ]
+number = ''.join(str(el) for el in lst)
+
+with open("test.txt", "w", encoding="UTF-8") as f:
+    f.write(number)
+f.close()
+
+with open("test.txt", "r", encoding="UTF-8") as f:
+    number = f.read()
+f.close()
+
+lst1 = []
+lst2 = []
+
+for i in range(10):
+    pattern = str(i) + "+"
+    lst1.append(max(re.findall(pattern, number)))
+
+count = max([len(i) for i in lst1])
+
+for el in lst1:
+    if len(el) == count:
+        lst2.append(el)
+
+print(f"Самые длинные последовательности длиной {count}: {lst2}")
