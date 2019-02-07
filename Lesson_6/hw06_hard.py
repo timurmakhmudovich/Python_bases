32039716df21f30424c0502d0d4f13496ce30502
timurmakhmudovich <itm1988@yandex.ru>
2019-02-07 11:20:04 +0300

    Сделал hard

--- Lesson_6/hw06_hard.py
+++ Lesson_6/hw06_hard.py
@@ -13,3 +13,47 @@
 каждый работник получал строку из файла
 
 '''
+
+import os
+
+path1 = os.path.join("data", "workers")
+path2 = os.path.join("data", "hours_of")
+
+with open(path1, 'r', encoding="UTF-8") as f:
+    workers = f.readlines()
+
+with open(path2, 'r', encoding="UTF-8") as f:
+    hours = f.readlines()
+
+class Worker():
+    def __init__(self, worker):
+        self.worker = worker.split()
+
+    def payout(self):
+        lst1 = self.worker
+        for el in hours:
+            if lst1[0] in el and lst1[1] in el:
+                if lst1[0] == "Имя":
+                    print("{:<10}{:<10}Выплата".format('Имя','Фамилия'))
+                else:
+                    lst2 = el.split()
+                    name = lst1[0]
+                    surname = lst1[1]
+                    salary = int(lst1[2])
+                    norm = int(lst1[4])
+                    real = int(lst2[2])
+
+                    if real == norm:
+                        payout = real
+                    elif real < norm:
+                        payout = salary * real / norm
+                    else:
+                        payout = salary + salary * (real - norm) * 2 / norm
+
+                    print(f"{name:<10}{surname:<10}{payout}")
+
+
+for data in workers:
+    worker = Worker(data)
+    worker.payout()
+
