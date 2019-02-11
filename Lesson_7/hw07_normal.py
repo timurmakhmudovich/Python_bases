7fce68861d6ada431926f9525b40d2439f5ff84f
timurmakhmudovich <itm1988@yandex.ru>
2019-02-11 18:39:08 +0300

    norm

--- Lesson_7/hw07_normal.py
+++ Lesson_7/hw07_normal.py
@@ -6,3 +6,34 @@
 Метод __add__ должен реализовывать сложение матриц, а __str__ - вывод итоговой
 матрицы.
 '''
+
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __add__(self, other):
+        if len(self.matrix) == len(other.matrix) and len(self.matrix[0]) == len(other.matrix[0]):
+            new_matrix = []
+            for i in range (0, len(self.matrix)):
+                new_matrix.append([])
+                for j in range (0, len(self.matrix[0])):
+                    new_matrix[i].append(self.matrix[i][j] + other.matrix[i][j])
+            return Matrix(new_matrix)
+        else:
+            return "Нельзя сколадывать матрицы разных размеров"
+
+    def __str__(self):
+        string = "Matrix:\n"
+        for row in self.matrix:
+            for el in row:
+                string += str(el) + "\t"
+            string += "\n"
+        return string
+
+
+
+
+matrix1 = Matrix([ [1,2,3], [4,5,6], [7,8,9] ])
+matrix2 = Matrix( [ [0,3,5], [7,9,11], [13,15,17] ])
+new_matrix = matrix1 + matrix2
+print(str(new_matrix))
